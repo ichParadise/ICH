0f223c0d2f4f880567a48df9a462f57b7a4cd218
ich <ich@email.com>
2015-12-01 10:07:22 +0800

    test

---
+++ 服务器/服务器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  服务器
+//
+//  Created by nctarena on 15/12/1.
+//  Copyright © 2015年 nctarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 服务器/服务器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  服务器
+//
+//  Created by nctarena on 15/12/1.
+//  Copyright © 2015年 nctarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
